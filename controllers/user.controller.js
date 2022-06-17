1cf6b9fa2dceb2ca212acbd7087f18e847ee0774
igorGavr <ragamuffinrogi@gmail.com>
2022-06-17 12:40:40 +0300

    express CRUD

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,70 +2,90 @@ const {fileService} = require("../services");
 
 module.exports = {
     allUsers: async (req, res) => {
-        const users = await fileService.reader()
-        res.json(users)  // -----
+        try{
+            const users = await fileService.reader()
+            res.json(users)  // -----
+        }catch (e) {
+            res.status(400).json(e.message || 'Unknown Error')
+        }
     },
 
     createUser: async (req, res) => {
-        const {name, age} = req.body
-        if (!Number.isInteger(age) || age < 18) {
-            return res.status(400).json('Set valid age')
+        try{
+            const {name, age} = req.body
+            if (!Number.isInteger(age) || age < 18) {
+                return res.status(400).json('Set valid age')
+            }
+            if (!name || name.length < 3) {
+                return res.status(400).json('Set valid name')
+            }
+            const users = await fileService.reader()  // отримуємо поточних Юзерів
+            // формуємо нового Юзера
+            const newUser = {...req.body, id: users.length ? users[users.length - 1].id + 1 : 1}
+            console.log(newUser)
+            await fileService.writer([...users, newUser]) // передаємо в метод поточних Юзерів та Нового
+            // Юзера в форматі _______
+            res.status(201).json(newUser)   // -------
+        }catch (e) {
+            res.status(400).json(e.message || 'Unknown Error')
         }
-        if (!name || name.length < 3) {
-            return res.status(400).json('Set valid name')
-        }
-        const users = await fileService.reader()  // отримуємо поточних Юзерів
-        // формуємо нового Юзера
-        const newUser = {...req.body, id: users.length ? users[users.length - 1].id + 1 : 1}
-        console.log(newUser)
-        await fileService.writer([...users, newUser]) // передаємо в метод поточних Юзерів та Нового
-        // Юзера в форматі _______
-        res.status(201).json(newUser)   // -------
     },
 
     getUserById: async (req, res) => {
-        const {userId} = req.params
-        const users = await fileService.reader()
-        const user = users.find((user) => user.id === +userId)
-        if (!user){
-            return res.status(400).json(`User with id ${userId} is not found`)
+        try{
+            const {userId} = req.params
+            const users = await fileService.reader()
+            const user = users.find((user) => user.id === +userId)
+            if (!user){
+                return res.status(400).json(`User with id ${userId} is not found`)
+            }
+            console.log(user)
+            res.json(user)  // -----
+        }catch (e) {
+            res.status(400).json(e.message || 'Unknown Error')
         }
-        console.log(user)
-        res.json(user)  // -----
     },
 
     updateUserById: async (req, res) => {
-        const {name, age} = req.body
-        const {userId} = req.params
+        try{
+            const {name, age} = req.body
+            const {userId} = req.params
 
-        if (age && !Number.isInteger(age) || age < 18) {
-            return res.status(400).json('Set valid age')
+            if (age && !Number.isInteger(age) || age < 18) {
+                return res.status(400).json('Set valid age')
+            }
+            if (name && name.length < 3) {
+                return res.status(400).json('Set valid name')
+            }
+            const users = await fileService.reader()  // отримуємо поточних Юзерів
+            const index = users.findIndex((user) => user.id === +userId)
+            if (index === -1) {
+                return res.status(400).json(`User with Id ${userId} is not found`)
+            }
+            const updatedUser = Object.assign(users[index], req.body)
+            // const updatedUser = {...users[index], ...req.body}
+            console.log(updatedUser)
+            users.splice(index, 1)
+            await fileService.writer([...users, updatedUser])
+            res.status(201).json(updatedUser)   // -------
+        }catch (e) {
+            res.status(400).json(e.message || 'Unknown Error')
         }
-        if (name && name.length < 3) {
-            return res.status(400).json('Set valid name')
-        }
-        const users = await fileService.reader()  // отримуємо поточних Юзерів
-        const index = users.findIndex((user) => user.id === +userId)
-        if (index === -1) {
-            return res.status(400).json(`User with Id ${userId} is not found`)
-        }
-        const updatedUser = Object.assign(users[index], req.body)
-        // const updatedUser = {...users[index], ...req.body}
-        console.log(updatedUser)
-        users.splice(index, 1)
-        await fileService.writer([...users, updatedUser])
-        res.status(201).json(updatedUser)   // -------
     },
 
     deleteUserById: async (req, res) => {
-        const {userId} = req.params
-        const users = await fileService.reader()
-        const index = users.findIndex((user) => user.id === +userId)
-        if (index === -1){
-            return res.status(400).json(`User with id ${userId} is not found`)
+        try{
+            const {userId} = req.params
+            const users = await fileService.reader()
+            const index = users.findIndex((user) => user.id === +userId)
+            if (index === -1){
+                return res.status(400).json(`User with id ${userId} is not found`)
+            }
+            users.splice(index, 1)
+            await fileService.writer(users)
+            res.sendStatus(204)  // -----
+        }catch (e) {
+            res.status(400).json(e.message || 'Unknown Error')
         }
-        users.splice(index, 1)
-        await fileService.writer(users)
-        res.sendStatus(204)  // -----
     }
 }
