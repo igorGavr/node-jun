6386fbb13c82d2b8791f781cb3340c5a2d7d6ac8
igorGavr <ragamuffinrogi@gmail.com>
2022-06-20 21:43:11 +0300

    mongoo

--- configs/configs.js
+++ configs/configs.js
@@ -1,4 +1,4 @@
 module.exports = {
     PORT: 5000,
-    MONGO_URL: 'mongodb://localhost:27017'
+    MONGO_URL: 'mongodb+srv://igorgavr:igorgavr@cluster0.xmdjroa.mongodb.net/?retryWrites=true&w=majority'
 };
