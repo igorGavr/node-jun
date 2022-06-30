bcddd52bcff637eaaecf0ff4e33c385bf7fa8cd3
igorGavr <ragamuffinrogi@gmail.com>
2022-06-30 14:52:03 +0300

    mailer dotenv nodemailer nodemon nodemailer-express-handlebars

--- configs/configs.js
+++ configs/configs.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 5555,
-    MONGO_URL: process.env.MONGO_URL || 'mongodb+srv://igorgavr:igorgavr@cluster0.xmdjroa.mongodb.net/?retryWrites=true&w=majority',
+    MONGO_URL: process.env.MONGO_URL || 'mongodb+srv://nadia:nadia@cluster0.faqsewc.mongodb.net/test',
 
     ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET || 'qwe', // КОДОВЕ слово повино бути мін 30 символів
     REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET || 'sad',
